2450d7a5bc16bf9a17c78fedb35bcf39095c6c9a
veesamsatyateja <veesam.satyateja@bijak.in>
2025-05-14 16:36:00 +0530

    publish changes

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
+  base: "/my-portfolio-verse/",
   server: {
     port: 5173,
   },
