4daba24d352a9fa118ab94cf5f430a43f5156e8d
veesamsatyateja <veesam.satyateja@bijak.in>
2025-05-14 16:25:45 +0530

    run server

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,13 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "::",
-    port: 8080,
+    port: 5173,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
@@ -20,3 +17,4 @@ export default defineConfig(({ mode }) => ({
     },
   },
 }));
+
